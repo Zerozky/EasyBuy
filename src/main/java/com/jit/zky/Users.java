b569ddbc849f852d91c76f6367cf27db3d2561c5
Zhu Keyu <zkyly@qq.com>
2023-10-20 10:39:20 +0800

    Create Users.java

---
+++ src/main/java/com/jit/zky/Users.java
@@ -0,0 +1,10 @@
+package com.jit.zky;
+
+public class Users {
+    private String username;
+    private String password;
+    private String name;
+    private String telephone;
+    private String email;
+
+}
